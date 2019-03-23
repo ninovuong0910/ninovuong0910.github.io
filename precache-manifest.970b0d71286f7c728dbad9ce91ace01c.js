9eb2a91e8e648b68da8af494dfea5adbd82f5db9
ninovuong0910 <47912735+ninovuong0910@users.noreply.github.com>
2019-03-23 15:33:00 +0700

    init project

---
+++ precache-manifest.970b0d71286f7c728dbad9ce91ace01c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "966426bb36f1600ab64a",
+    "url": "/static/js/main.966426bb.chunk.js"
+  },
+  {
+    "revision": "1260d9ef32c25c17252d",
+    "url": "/static/js/2.1260d9ef.chunk.js"
+  },
+  {
+    "revision": "966426bb36f1600ab64a",
+    "url": "/static/css/main.855b1661.chunk.css"
+  },
+  {
+    "revision": "b8672804372e2d7ef2afb7a8e25a5af3",
+    "url": "/index.html"
+  }
+];
